34334970bdacd9c0b885763cbbc88e759254bc95
Oier Urkizu <urkizu.oier@gmail.com>
2025-01-07 18:38:53 +0100

    proveedor y vehiculo sin detaller hecho

---
+++ WebApplicationSample/src/java/G3/crud/entities/Compra.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package G3.crud.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 2dam
+ */
+@Entity
+@Table(name="compra",schema="pruebadb")
+@XmlRootElement
+public class Compra implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+ 
+    @EmbeddedId
+    private CompraId idCompra;
+    @MapsId("usuarioId")
+    @JoinColumn(name = "usuarioId", referencedColumnName = "idPersona")
+    @ManyToOne
+    private Usuario usuario;
+    @MapsId("vehiculoId")
+    @JoinColumn(name = "vehiculoId", referencedColumnName = "idVehiculo")
+    @ManyToOne
+    private Vehiculo vehiculo;
+    
+    
+    
+    private String matricula;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCompra;
+    
+    
+    public CompraId getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(CompraId idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }   
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCompra != null ? idCompra.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Compra)) {
+            return false;
+        }
+        Compra other = (Compra) object;
+        if ((this.idCompra == null && other.idCompra != null) || (this.idCompra != null && !this.idCompra.equals(other.idCompra))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "G3.crud.entities.Compra[ idCompra=" + idCompra + " ]";
+    }
+    
+}
